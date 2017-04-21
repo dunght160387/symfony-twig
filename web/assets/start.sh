c1a557814b9e5491f82edeaeb016f3c87fb324a1
Dzung Ha The <dunght163@gmail.com>
2017-04-21 22:16:05 +0700

    Add HomeBundle, add use lib creative for HomeBundle

--- web/assets/start.sh
+++ web/assets/start.sh
@@ -2,3 +2,4 @@
 ln -sf ../../bower_components/bootstrap bootstrap
 ln -sf ../../bower_components/jquery jquery
 ln -sf ../../lib/startbootstrap-sb-admin-2-gh-pages sb-admin-2
+ln -sf ../../lib/startbootstrap-creative-gh-pages creative
